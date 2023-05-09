26a64546b52fbe6bd6aad8b2a1e50ba2a341ef4f
Stas <staspolevechko@gmail.com>
2023-05-09 19:26:38 +0300

    задача 29 готова

---
+++ z3/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readInput = System.Console.ReadLine() ?? "";
+    int result = int.Parse(readInput);
+    return result;
+}
+int[] GenerateArray(int Length, int minValue, int maxValue)
+{
+    int[] array = new int[Length];
+    Random random = new Random();
+    for (int i = 0; i < Length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+
+    }
+    System.Console.Write($"{array[array.Length - 1]} ");
+    System.Console.Write("]");
+}
+int length = Prompt("количество элементов в массиве: ");
+int min = Prompt("минимальное значение массива: ");
+int max = Prompt("максимальное значение массива: ");
+int[] array = GenerateArray(length, min, max);
+PrintArray(array);
